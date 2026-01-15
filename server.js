3d20b07ec32c5374c5fda5288980881cdb08ebd0
abhishek8676-pro <abhymanu8676@gmail.com>
2026-01-16 02:35:42 +0530

    server.js can replace from src/server.js and set to main:server.js

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 // Entry: connects DB and starts the server
 require('dotenv').config();
 const http = require('http');
-const app = require('./app');
-const { connectDB } = require('./config/db');
-const logger = require('./utils/logger');
+const app = require('./src/app');
+const { connectDB } = require('./src/config/db');
+const logger = require('./src/utils/logger');
 
 const PORT = process.env.PORT || 4000;
 
@@ -20,4 +20,12 @@ async function start() {
   }
 }
 
-start();
+if (require.main === module) {
+  start();
+}
+
+// Export for Vercel
+module.exports = async (req, res) => {
+  await connectDB();
+  return app(req, res);
+};
